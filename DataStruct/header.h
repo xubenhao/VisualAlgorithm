9097ed0fe1f2b85e2e41e6a840e8dacfde55fd90
XuBenHao <xbh370970843@163.com>
2021-01-27 09:43:25 +0800

    header

---
+++ DataStruct/header.h
@@ -0,0 +1,8 @@
+//	Author : XuBenHao                                                  
+//	Version : 1.0.0                 
+//	Mail : xbh370970843@163.com
+//	Copyright : XuBenHao 2020 - 2030 
+#ifndef DATASTRUCT_HEADER_H
+#define DATASTRUCT_HEADER_H
+#include "Global/lib.h"
+#endif 
