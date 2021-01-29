fe3d2da2d5b36bdb41f19da0386589a86f7c17c3
XuBenHao <xbh370970843@163.com>
2021-01-29 10:02:43 +0800

    lib

---
+++ Geometry/lib.h
@@ -0,0 +1,11 @@
+#ifndef GEOMETRY_LIB_H
+#define GEOMETRY_LIB_H
+#include "geometry.h"
+#include "geometrypoint.h"
+#include "geometryline.h"
+#include "geometryrect.h"
+#include "geometryellipse.h"
+#include "geometrypolyline.h"
+#include "geometrytext.h"
+#include "geometrygroup.h"
+#endif // LIB_H
