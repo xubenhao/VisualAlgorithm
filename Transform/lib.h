fee979a02526c70f6a0a532fcd480bd9b28b3c99
XuBenHao <xbh370970843@163.com>
2021-01-30 09:45:45 +0800

    lib

---
+++ Transform/lib.h
@@ -0,0 +1,9 @@
+#ifndef TRANSFORM_LIB_H
+#define TRANSFORM_LIB_H
+#include "transform.h"
+#include "lineartransform.h"
+#include "rotatetransform.h"
+#include "scaletransform.h"
+#include "matrixtransform.h"
+#include "transformgroup.h"
+#endif // LIB_H
