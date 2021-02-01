c8c60480fcea5acb2574ed85571cb02c89967e95
XuBenHao <xbh370970843@163.com>
2021-02-01 09:51:14 +0800

    ncparameter

---
+++ Parameter/NcParameter/ncparameter.cpp
@@ -0,0 +1,14 @@
+//	Author : XuBenHao      
+//	Version : 1.0.0                 
+//	Mail : xbh370970843@163.com
+//	Copyright : XuBenHao 2020 - 2030
+
+#include "ncparameter.h"
+
+namespace NParameter
+{
+    NcParameter::NcParameter()
+    {
+
+    }
+}
---
+++ Parameter/NcParameter/ncparameter.h
@@ -0,0 +1,19 @@
+//	Author : XuBenHao      
+//	Version : 1.0.0                 
+//	Mail : xbh370970843@163.com
+//	Copyright : XuBenHao 2020 - 2030
+
+#ifndef PARAMETER_NCPARAMETER_H
+#define PARAMETER_NCPARAMETER_H
+
+#include "Parameter/parameter.h"
+namespace NParameter
+{
+    class NcParameter : public Parameter
+    {
+    public:
+        NcParameter();
+    };
+}
+
+#endif // NCPARAMETER_H
