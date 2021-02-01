51704e4060482a896ae57c793d2b453dbf5a64d3
XuBenHao <xbh370970843@163.com>
2021-02-01 10:07:19 +0800

    lib

---
+++ FileGenerate/lib.h
@@ -0,0 +1,8 @@
+#ifndef FILE_GENERATE_LIB_H
+#define FILE_GENERATE_LIB_H
+#include "filegenerate.h"
+#include "ModelFileGenerate/modelfilegenerate.h"
+#include "ModelFileGenerate/arraymodelfilegenerate.h"
+#include "NcFileGenerate/ncfilegenerate.h"
+#include "NcFileGenerate/arrayncfilegenerate.h"
+#endif // LIB_H
