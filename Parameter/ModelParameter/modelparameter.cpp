4208b28fc79045c4b050cce8a7bba3d617833ebc
XuBenHao <xbh370970843@163.com>
2021-02-01 09:46:18 +0800

    modelparameter

---
+++ Parameter/ModelParameter/modelparameter.cpp
@@ -0,0 +1,16 @@
+//	Author : XuBenHao      
+//	Version : 1.0.0                 
+//	Mail : xbh370970843@163.com
+//	Copyright : XuBenHao 2020 - 2030
+
+#include "modelparameter.h"
+
+namespace NParameter
+{
+    ModelParameter::ModelParameter()
+    {
+
+    }
+
+
+}
---
+++ Parameter/ModelParameter/modelparameter.h
@@ -0,0 +1,20 @@
+//	Author : XuBenHao      
+//	Version : 1.0.0                 
+//	Mail : xbh370970843@163.com
+//	Copyright : XuBenHao 2020 - 2030
+
+#ifndef PARAMETER_MODELPARAMETER_H
+#define PARAMETER_MODELPARAMETER_H
+#include "Parameter/parameter.h"
+
+namespace NParameter
+{
+    class ModelParameter : public Parameter
+    {
+    public:
+        ModelParameter();
+    };
+}
+
+
+#endif // MODELPARAMETER_H
