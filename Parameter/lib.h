a92faceba9fc0c63fbd030102f8977fe3da8b955
XuBenHao <xbh370970843@163.com>
2021-02-01 09:54:00 +0800

    lib

---
+++ Parameter/lib.h
@@ -0,0 +1,8 @@
+#ifndef PARAMETER_LIB_H
+#define PARAMETER_LIB_H
+#include "parameter.h"
+#include "ModelParameter/modelparameter.h"
+#include "ModelParameter/arraymodelparameter.h"
+#include "NcParameter/ncparameter.h"
+#include "NcParameter/arrayncparameter.h"
+#endif // LIB_H
