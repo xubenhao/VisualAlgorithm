0497063f6307db23ff89148c59e4cc0ad4aad837
XuBenHao <xbh370970843@163.com>
2021-02-02 09:51:31 +0800

    header

---
+++ Sync/header.h
@@ -0,0 +1,22 @@
+//	Author : XuBenHao      
+//	Version : 1.0.0                 
+//	Mail : xbh370970843@163.com
+//	Copyright : XuBenHao 2020 - 2030
+
+#ifndef SYNC_HEADER_H
+#define SYNC_HEADER_H
+#include "Global/lib.h"
+#include "DataStruct/lib.h"
+
+namespace NSyn
+{
+    enum OBJECT_STATE
+    {
+        NOT_START,
+        RUNNING,
+        NORMALLY_END,
+        ABNORMALLY_END,
+    };
+
+}
+#endif // HEADER_H
